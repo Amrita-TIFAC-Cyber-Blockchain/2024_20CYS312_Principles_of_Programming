90ff2bb427537e52fa76eb835b06e4485e0cf549
Ramaguru Radhakrishnan <7790256+ramagururadhakrishnan@users.noreply.github.com>
2024-04-27 21:30:56 +0530

    Solutions Folder Added
    - Mid-Term with Q10 (4 Variants)

---
+++ Assets/Solutions/Mid-Term/Q10/V1.hs
@@ -0,0 +1,12 @@
+findMax :: [Int] -> Int
+findMax [] = error "Empty list"
+findMax [x] = x
+findMax (x:xs) = max x (findMax xs)
+
+main :: IO ()
+main = do
+    let list1 = [1, -5, -3, 9, -2]
+    let list2 = [117, 12, 81, 14, 16]
+    putStrLn "Testing findMax function:"
+    putStrLn $ "List 1: " ++ show list1 ++ ", Maximum: " ++ show (findMax list1)
+    putStrLn $ "List 2: " ++ show list2 ++ ", Maximum: " ++ show (findMax list2)
---
+++ Assets/Solutions/Mid-Term/Q10/V3.hs
@@ -0,0 +1,13 @@
+findMax :: [Int] -> Int
+findMax [x] = x
+findMax (x:xs)
+    | x > findMax xs = x
+    | otherwise   = findMax xs
+		
+main :: IO ()
+main = do
+    let list1 = [1, -5, -3, 9, -2]
+    let list2 = [117, 12, 81, 14, 16]
+    putStrLn "Testing findMax function:"
+    putStrLn $ "List 1: " ++ show list1 ++ ", Maximum: " ++ show (findMax list1)
+    putStrLn $ "List 2: " ++ show list2 ++ ", Maximum: " ++ show (findMax list2)
